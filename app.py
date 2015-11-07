4a2423301c4890d2b45fa73ca7f99096fbfeabfd
Nick Tikhonov <nt34@st-andrews.ac.uk>
2015-11-07 17:14:46 +0000

    Add rethink

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
 from flask import Flask, request, redirect, jsonify, render_template
-import os
 import twilio.twiml
 import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError
 
 app = Flask(__name__)
+DB = "dbase"
+
+# db setup; only run once
+def dbSetup():
+    connection = r.connect(host="localhost", port=28015)
+    try:
+        r.db_create(DB).run(connection)
+        r.db(DB).table_create('inspections').run(connection)
+        r.db(DB).table_create('harvests').run(connection)
+        r.db(DB).table_create('hives').run(connection)
+        r.db(DB).table_create('apiaries').run(connection)
+        print 'Database setup completed'
+    except RqlRuntimeError:
+        print 'Database already exists.'
+    finally:
+        connection.close()
+dbSetup()
+
+@app.before_request
+def before_request():
+    try:
+        g.rdb_conn = r.connect(host="localhost", port=28015, db=DB)
+    except RqlDriverError:
+        abort(503, "Database connection could be established.")
+
+@app.teardown_request
+def teardown_request(exception):
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
 
 @app.route('/')
 def hello():
