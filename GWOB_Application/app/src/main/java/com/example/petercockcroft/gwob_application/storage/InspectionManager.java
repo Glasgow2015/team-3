317680f8d9de32ea401de307e8c54884a219442f
Robin Nabel <rnabel@ucdavis.edu>
2015-11-07 22:09:49 +0000

    Splash screens finished. Linking done.

---
+++ GWOB_Application/app/src/main/java/com/example/petercockcroft/gwob_application/storage/InspectionManager.java
@@ -0,0 +1,9 @@
+package com.example.petercockcroft.gwob_application.storage;
+
+/**
+ * Created by robin on 07/11/15.
+ */
+public class InspectionManager {
+    //
+    String[] codes = new String[]{"notinuse","notoccupied","occupied","absconded","robbed","honeybadgered","mites","beetle","ants","fire","flood", "justdead"};
+}
