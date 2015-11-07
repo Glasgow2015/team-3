30ed18155381700852f9764fa87e3b9a6fc2c5d8
Robin Nabel <rnabel@ucdavis.edu>
2015-11-07 20:01:58 +0000

    Updated forms. Now fully localized.

--- GWOB_Application/app/src/main/java/com/example/petercockcroft/gwob_application/HarvestForm.java
+++ GWOB_Application/app/src/main/java/com/example/petercockcroft/gwob_application/HarvestForm.java
@@ -61,7 +61,7 @@ public class HarvestForm extends SingleInputFormActivity {
         // Quantity of ripe combs.
         steps.add(
                 new TextStep(context, DATA_KEY_RIPE_COMBS,
-                        InputType.TYPE_NUMBER_VARIATION_NORMAL,
+                        InputType.TYPE_CLASS_NUMBER,
                         R.string.harvest_quant_title,
                         R.string.harvest_quant_error,
                         R.string.harvest_quant_detail,
@@ -86,79 +86,126 @@ public class HarvestForm extends SingleInputFormActivity {
         // Protective clothing available for beekeeper YES/NO.
         steps.add(
                 new OptionStep(context, DATA_KEY_CLOTH_BEE,
-                        new String[]{"Yes", "No"},
+                        new String[]{
+                                getResources().getString(R.string.boolean_positive),
+                                getResources().getString(R.string.boolean_negative)
+                        },
                         R.string.harvest_cloth_bee_title,
+                        R.string.harvest_cloth_bee_error,
+                        R.string.harvest_cloth_bee_detail
+                )
+        );
+
+        // Protective clothing available for all assistants YES/NO.
+        steps.add(
+                new OptionStep(context, DATA_KEY_CLOTH_ASSIST,
+                        new String[]{
+                                getResources().getString(R.string.boolean_positive),
+                                getResources().getString(R.string.boolean_negative)
+                        },
+                        R.string.harvest_cloth_assist_title,
                         R.string.harvest_cloth_assist_error,
                         R.string.harvest_cloth_assist_detail
                 )
         );
 
-        // Protective clothing available for all assistants YES/NO.
-
         // Smoker available, YES/NO.
+        steps.add(
+                new OptionStep(context, DATA_KEY_SMOKER,
+                        new String[]{
+                                getResources().getString(R.string.boolean_positive),
+                                getResources().getString(R.string.boolean_negative)
+                        },
+                        R.string.harvest_smoker_title,
+                        R.string.harvest_smoker_error,
+                        R.string.harvest_smoker_detail
+                )
+        );
 
         // Number of clean, airtight buckets available for harvest.
-
-
         steps.add(
-                new CheckBoxStep(context, DATA_KEY_EULA, R.string.eula, R.string.eula_title, R.string.eula_error, R.string.eula_details, new CheckBoxStep.StepChecker() {
-                    @Override
-                    public boolean check(boolean input) {
-                        return input;
-                    }
-                })
-        );
-        steps.add(
-                new TextStep(context, DATA_KEY_EMAIL, InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS, R.string.email, R.string.email_error, R.string.email_details, new TextStep.StepChecker() {
-                    @Override
-                    public boolean check(String input) {
-                        return android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches();
-                    }
-                })
-        );
-        steps.add(
-                new TextStep(context, DATA_KEY_PASSWORD, InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD, R.string.password, R.string.password_error, R.string.password_details, new TextStep.StepChecker() {
-                    @Override
-                    public boolean check(String input) {
-                        return input.length() >= 5;
-                    }
-                })
-        );
-        steps.add(
-                new DateStep(context, DATA_KEY_BIRTHDAY, R.string.birthday, R.string.birthday_error, R.string.birthday_details, new DateStep.StepChecker() {
-                    @Override
-                    public boolean check(int year, int month, int day) {
-                        Calendar today = new GregorianCalendar();
-                        Calendar birthday = new GregorianCalendar(year, month, day);
-                        today.add(Calendar.YEAR, -14);
-                        return today.after(birthday);
-                    }
-                })
-        );
-        steps.add(
-                new SeekBarStep(context, DATA_KEY_HEIGHT, 150, 180, R.string.height, R.string.height_error, R.string.height_details, new SeekBarStep.StepChecker() {
-                    @Override
-                    public boolean check(int progress) {
-                        return progress >= 160;
-                    }
-                })
-        );
-        steps.add(
-                new TextStep(context, DATA_KEY_CITY, InputType.TYPE_CLASS_TEXT, R.string.city, R.string.city_error, R.string.city_details)
+                new TextStep(context, DATA_KEY_BUCKETS,
+                        InputType.TYPE_CLASS_NUMBER,
+                        R.string.harvest_num_buck_title,
+                        R.string.harvest_num_buck_error,
+                        R.string.harvest_num_buck_detail,
+                        new TextStep.StepChecker() {
+                            @Override
+                            public boolean check(String input) {
+                                int result;
+                                try {
+                                    result = Integer.parseInt(input);
+                                } catch(NumberFormatException e) {
+                                    return false;
+                                } catch(NullPointerException e) {
+                                    return false;
+                                }
+                                // Check if positive.
+                                if (result < 0) return false;
+                                else return true;
+                            }
+                        })
         );
 
+//        steps.add(
+//                new CheckBoxStep(context, DATA_KEY_EULA, R.string.eula, R.string.eula_title, R.string.eula_error, R.string.eula_details, new CheckBoxStep.StepChecker() {
+//                    @Override
+//                    public boolean check(boolean input) {
+//                        return input;
+//                    }
+//                })
+//        );
+//        steps.add(
+//                new TextStep(context, DATA_KEY_EMAIL, InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS, R.string.email, R.string.email_error, R.string.email_details, new TextStep.StepChecker() {
+//                    @Override
+//                    public boolean check(String input) {
+//                        return android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches();
+//                    }
+//                })
+//        );
+//        steps.add(
+//                new TextStep(context, DATA_KEY_PASSWORD, InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD, R.string.password, R.string.password_error, R.string.password_details, new TextStep.StepChecker() {
+//                    @Override
+//                    public boolean check(String input) {
+//                        return input.length() >= 5;
+//                    }
+//                })
+//        );
+//        steps.add(
+//                new DateStep(context, DATA_KEY_BIRTHDAY, R.string.birthday, R.string.birthday_error, R.string.birthday_details, new DateStep.StepChecker() {
+//                    @Override
+//                    public boolean check(int year, int month, int day) {
+//                        Calendar today = new GregorianCalendar();
+//                        Calendar birthday = new GregorianCalendar(year, month, day);
+//                        today.add(Calendar.YEAR, -14);
+//                        return today.after(birthday);
+//                    }
+//                })
+//        );
+//        steps.add(
+//                new SeekBarStep(context, DATA_KEY_HEIGHT, 150, 180, R.string.height, R.string.height_error, R.string.height_details, new SeekBarStep.StepChecker() {
+//                    @Override
+//                    public boolean check(int progress) {
+//                        return progress >= 160;
+//                    }
+//                })
+//        );
+//        steps.add(
+//                new TextStep(context, DATA_KEY_CITY, InputType.TYPE_CLASS_TEXT, R.string.city, R.string.city_error, R.string.city_details)
+//        );
+//
         return steps;
     }
 
     @Override
     protected void onFormFinished(Bundle data) {
         Toast.makeText(this, "Form finished: " +
-                        CheckBoxStep.checked(data, DATA_KEY_EULA) + ", " +
-                        TextStep.text(data, DATA_KEY_EMAIL) + ", " +
-                        TextStep.text(data, DATA_KEY_PASSWORD) + ", " +
-                        DateStep.day(data, DATA_KEY_BIRTHDAY) + "." + DateStep.month(data, DATA_KEY_BIRTHDAY) + "." + DateStep.year(data, DATA_KEY_BIRTHDAY) + ", " +
-                        SeekBarStep.progress(data, DATA_KEY_HEIGHT) + ", " +
-                        TextStep.text(data, DATA_KEY_CITY),
+                        DateStep.text(data, DATA_KEY_DATE) + ", " +
+                        TextStep.text(data, DATA_KEY_RIPE_COMBS) + ", " +
+                        OptionStep.text(data, DATA_KEY_CLOTH_BEE) + ", " +
+                        OptionStep.text(data, DATA_KEY_CLOTH_ASSIST) + ", " +
+                        OptionStep.text(data, DATA_KEY_SMOKER) + ", " +
+                        TextStep.text(data, DATA_KEY_BUCKETS),
                 Toast.LENGTH_LONG).show();
         Log.d("MainActivity", "data: " + data.toString());
     }
