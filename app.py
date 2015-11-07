5e200cf93e19272534c4af4c1b34e4bb451486dc
Nick Tikhonov <nt34@st-andrews.ac.uk>
2015-11-07 16:29:27 +0000

    Add twilio endpoint

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
+from flask import Flask, jsonify
 import os
-from flask import Flask
+import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'python/flask'
+
+
+@app.route("/twilio", methods=['GET','POST'])
+def twilio_response():
+    try:
+        phone_number = request.values.get('From')
+        message_body = request.values.get('Body')
+
+        response = "YOLO: {}".format(message_body)
+
+        resp = twilio.twiml.Response()
+        resp.message(response)
+        return str(resp)
+    except Exception as e:
+        resp = twilio.twiml.Response()
+        resp.message("Sorry :(! An error occurred, please try again later.")
+        return resp
